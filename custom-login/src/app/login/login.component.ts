2109e3ad026146737f16a67170c63a80cc8124e3
Dragos Gaftoneanu <dragos.gaftoneanu@gmail.com>
2021-02-01 04:47:16 +0200

    Added FB IDP

--- custom-login/src/app/login/login.component.ts
+++ custom-login/src/app/login/login.component.ts
@@ -11,9 +11,11 @@
  */
 
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 import * as OktaSignIn from '@okta/okta-signin-widget';
 import sampleConfig from '../app.config';
 
+const DEFAULT_ORIGINAL_URI = window.location.origin;
 
 @Component({
   selector: 'app-login',
@@ -22,10 +24,10 @@ import sampleConfig from '../app.config';
 })
 export class LoginComponent implements OnInit {
   signIn: any;
-  constructor() {
+  constructor(public oktaAuth: OktaAuthService) {
     this.signIn = new OktaSignIn({
       /**
-       * Note: when using the Sign-In Widget for an ODIC flow, it still
+       * Note: when using the Sign-In Widget for an OIDC flow, it still
        * needs to be configured with the base URL for your Okta Org. Here
        * we derive it from the given issuer for convenience.
        */
@@ -39,28 +41,35 @@ export class LoginComponent implements OnInit {
         },
       },
       authParams: {
-        pkce: true,
-        responseMode: 'query',
-        issuer: sampleConfig.oidc.issuer,
-        display: 'page',
-        scopes: sampleConfig.oidc.scopes,
+        issuer: sampleConfig.oidc.issuer
       },
+	  idps: [
+		{type: 'FACEBOOK', id: '0oa9q2jgfwvDxkxUL2p7'}
+		]
     });
   }
 
   ngOnInit() {
-    this.signIn.renderEl(
-      { el: '#sign-in-widget' },
-      () => {
-        /**
-         * In this flow, the success handler will not be called because we redirect
-         * to the Okta org for the authentication workflow.
-         */
-      },
-      (err) => {
-        throw err;
-      },
-    );
+    this.signIn.showSignInToGetTokens({
+      el: '#sign-in-widget',
+      scopes: sampleConfig.oidc.scopes
+    }).then(tokens => {
+      // When navigating to a protected route, the route path will be saved as the `originalUri`
+      // If no `originalUri` has been saved, then redirect back to the app root
+      const originalUri = this.oktaAuth.getOriginalUri();
+      if (originalUri === DEFAULT_ORIGINAL_URI) {
+        this.oktaAuth.setOriginalUri('/');
+      }
+
+      // Remove the widget
+      this.signIn.remove();
+
+      // In this flow the redirect to Okta occurs in a hidden iframe
+      this.oktaAuth.handleLoginRedirect(tokens);
+    }).catch(err => {
+      // Typically due to misconfiguration
+      throw err;
+    });
   }
 
 }
